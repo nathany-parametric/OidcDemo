0bc8a692552452e5982eecbe1897d38f08f5bb68
Nathan Yang <nyang@paraport.com>
2019-08-01 12:05:04 -0700

    first commit

---
+++ OidcDemo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OidcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Index");
+        }
+
+        [Authorize]
+        public IActionResult Secured()
+        {
+            //var items = HttpContext.AuthenticateAsync().Result.Properties.Items;
+            var accessToken = HttpContext.GetTokenAsync("access_token").Result;
+            var idToken = HttpContext.GetTokenAsync("id_token").Result;
+            return Content($"Secured. accessToken: {accessToken}. idToken: {idToken}");
+        }
+    }
+}
---
+++ OidcDemo/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OidcDemo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+                    options.RequireHttpsMetadata = false;
+                    options.ResponseType = "code id_token";
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    //Authorization Server
+                    options.Authority = "https://parametricdev.oktapreview.com/oauth2/ausmmaqw8el5UZxzQ0h7";
+
+                    //Application (Client)
+                    options.ClientId = "0oamm5e373BR1LbZ30h7";
+                    options.ClientSecret = "MzlEguDLpg-PcNRfwghWJgF6qe7pHmLeLF18hJXv";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseAuthentication();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
